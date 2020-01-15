f644e33e47da286ec070f68a313ba61cb6f41fe2
Dave0512 <dschneiderhoffmann@gmail.com>
2020-01-15 18:55:55 +0100

    Added methods to compare objects

--- vehicles.py
+++ vehicles.py
@@ -14,24 +14,43 @@ class Vehicle:
 
     # Methods
     def speed_up(self,value):
+        """
+        method to speed up an object
+        """
         self.speed += value
 
     def execute(self):
+        """
+        main def
+        """
         print("Speed:",self.speed)
+
+    # Methods to compare objects
+    def __gt__(self,other):
+        """
+        first method to compare objects
+        """
+        return self.speed > other.speed
+
+    def __eq__(self,other):
+        """
+        first method to compare objects
+        """
+        return self.speed == other.speed
 
 
 if __name__ == "__main__":
-    opel = Vehicle("Opel",60)
-    volvo = Vehicle("Volvo",70)
-    
+    opel = Vehicle("Opel",31)
+    volvo = Vehicle("Volvo",50)
+
     opel.execute()
     opel.speed_up(20)
     opel.execute()
-    
+
     # Compare objects
     if opel > volvo:
         print("Opel is faster")
     elif opel == volvo:
         print("Both cars have the same speed!")
-    else: 
+    else:
         print("Volvo is faster")
