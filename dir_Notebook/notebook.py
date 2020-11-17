d8cebbb21897bd513fe2e78dcf9f699f34604c77
Dave0512 <dschneiderhoffmann@gmail.com>
2020-11-17 11:38:33 +0100

    Type: Class - Note added

--- dir_Notebook/notebook.py
+++ dir_Notebook/notebook.py
@@ -1,3 +1,11 @@
 import datetime
 
 # store the next available id for all new notes
+last_id = 0
+
+class Note:
+    """
+    Represent a note in a notebook. Match against a
+    string in searches and staore tags for each note.
+    """
+    pass
