e3ca4b0dc116a1912222ec3ba9583942e316f395
Dave0512 <dschneiderhoffmann@gmail.com>
2020-11-17 11:42:43 +0100

    Type: def - init with default and autom. filled values

--- dir_Notebook/notebook.py
+++ dir_Notebook/notebook.py
@@ -8,4 +8,16 @@ class Note:
     Represent a note in a notebook. Match against a
     string in searches and staore tags for each note.
     """
-    pass
+    def __init__(self,memo,tags=''):
+        """
+        Initialize a note with memo and optional 
+        space-seperated tags.
+        Automatically set the note's creation date
+        and a unique id.
+        """
+        self.memo = memo
+        self.tags = tags
+        self.creation_date = datetime.date.today()
+        global last_id
+        last_id += 1
+        self.id = last_id 
