37460c0a8836d2ddb6afdf359fa753beb8be7d03
Dave0512 <dschneiderhoffmann@gmail.com>
2020-11-19 14:41:30 +0100

    Type: def - init added

--- dir_Database/database.py
+++ dir_Database/database.py
@@ -2,7 +2,23 @@ import pyodbc
 
 class Database:
     """
-    Class to connect to several types of relational dbms
+    Class to connect, and interact with several types of relational dbms
     like ms sql server, mySQL, PostgreSQL, SQLite
     """
-    pass
+    def __init__(self,driver, server, database,tr_conn):
+        """
+        Method recieves compulsory parameter 
+        to create a connection to a database.
+
+        MS SQL Server, for instance:
+        "DRIVER={SQL Server Native Client 11.0};"
+        "SERVER=192.168.16.124;"
+        "DATABASE=Vorlauf_DB;"
+        "Trusted_Connection=yes;
+        """
+        self._db_conn = pyodbc.connect(r"DRIVER={0};"
+                                           "SERVER={1};"
+                                           "DATABASE={2};"
+                                           "Trusted_Connection={3};".format(driver, server, database,tr_conn))
+
+    
