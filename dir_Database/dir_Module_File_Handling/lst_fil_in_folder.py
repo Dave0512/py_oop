447cdbe592f7372f11ebce3a535727d3580d3bbe
Dave0512 <dschneiderhoffmann@gmail.com>
2020-12-02 14:16:02 +0100

    Type: def - filterFileList returns str.split: only list with fileNames without path

--- dir_Database/dir_Module_File_Handling/lst_fil_in_folder.py
+++ dir_Database/dir_Module_File_Handling/lst_fil_in_folder.py
@@ -1,6 +1,7 @@
 from glob import glob
 import os
 import glob
+import pandas as pd
 
 
 class FileList(list):
@@ -18,31 +19,52 @@ class FileList(list):
         suffix= "xls"
     """
     
-    def __init__(self,pfad="", suffix="xls"):
+    def __init__(self,pfad="", suffix="xls",kriteriumIdentifikationDatei="Bewegungsdaten"):
         """
 
         """
         self._pfad = pfad
         self._suffix = suffix
+        self._kriteriumIdentifikationDatei = kriteriumIdentifikationDatei
 
-    def searchFile(self): # , tableName, headerCell):
+    def createFileList(self): # , tableName, headerCell):
         """
-        returns all fileNames that contains the suffix, tableName and headerCell
+        returns list of all fileNames that contains the suffix in the tree-folders
         """
         fileList = []
         fileList = glob.glob("{0}**/*{1}*".format(self._pfad,self._suffix),recursive=True)
         return fileList
 
+    def filterFileList(self):
+        """
+        input: List of files in folder-tree
+        output: Filtered list of files. Condition is the file has a Sheet named = "wichtigesBlatt"
+        """
+        # 3) Identifiziere Tabelle "Bewegungsdaten"
+        filterKriterium = self._kriteriumIdentifikationDatei
+        lstAllg = self.createFileList()
+        lstHCSRFiles = []
+        for file in lstAllg:
+            xl = pd.ExcelFile(file) 
 
-# Test 
-Lister = FileList("Z:\\1_AGKAMED_Arbeit\\0_GIT_REPOS\\1_ETL\\","ipynb")
+            lstWs = xl.sheet_names
+            for sheet in lstWs:
+                if str(sheet) == filterKriterium:
+                    lstHCSRFiles.append(file.split("\\")[-1])  
+        
+        return lstHCSRFiles
 
-print(Lister.searchFile())
+    def designFilteredFileList(self):
+        pass
+        # lstFilt = filterFileList
 
-# 1) Liste Exceldateien in spezifischen Ordner 
+
+
+# 1) Liste Exceldateien in spezifischen Ordner - ERLEDIGT
 # 2) Öffne Excel (Oder xml, csv)
-# 3) Identifiziere Tabelle "Bewegungsdaten"
-# 4) Identifiziere Überschriftenzeile
+# 3) Identifiziere Tabelle "Bewegungsdaten" - ERLEDIGT
+# 3.1) Erstelle Liste mit HCSR Dateien
+# 4) Identifiziere Überschriftenzeile 
 # 5) Wenn alles vorhanden - Schreibe in liste (DB Tabelle) und übertrage in SQL DB
 # 6) Wenn nicht alles vorhanden - liste Excelfiles auf bei denen es fehlt
 
