edcbcb57138e131fcc53d4db81bbcf89d81a23de
Dave0512 <dschneiderhoffmann@gmail.com>
2021-02-02 11:24:09 +0100

    Type: PyQt App

---
+++ dir_Database/frontend.py
@@ -0,0 +1,21 @@
+
+
+import sys 
+import os 
+import os.path
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+import pyodbc
+import urllib
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlStrings import headLieferanten
+import pandas as pd
+import datetime as dt
+
+from queryTemplate import Conn_DB
+from lst_fil_in_folder import FileList
+from dfDesign import TableToDF, DfDesignerPiv
+from dfFromList import ListToDF
