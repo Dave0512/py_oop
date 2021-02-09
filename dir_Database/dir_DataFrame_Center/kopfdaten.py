42b16ba9124f9062cd083fb3077e5d032825c3d3
Dave0512 <dschneiderhoffmann@gmail.com>
2021-02-09 17:15:10 +0100

    Type: Structure of Handling Kopfdaten

--- dir_Database/dir_DataFrame_Center/kopfdaten.py
+++ dir_Database/dir_DataFrame_Center/kopfdaten.py
@@ -1,16 +1,24 @@
 
 
 import pandas as pd
-
-import xlrd
+import openpyxl
+from openpyxl import load_workbook
 from datetime import datetime
+
+# STRUKTUR
+# 1) Dynamisch durch Dateiliste iterieren
+# 2) PRÜFUNG: Tabellenblätter vorhanden?
+# 3) Tabelle Kopfdaten als ws einladen
+# 4) PRÜFUNG: datumVon & datumBis durchführen
+# 5) Dict mit Zellinhalten erstellen
+# 6) Inhalte mit dateiNamen in DB schreiben
+
 dateiName = "01_2020_Health Care Sales Report V2.1_Abbott Medical_AGKAMED.xlsm"
 
 # ##############
 # TESTE openpyxl
 # ##############
-import openpyxl
-from openpyxl import load_workbook
+
 wb_openpyxl = openpyxl.load_workbook(dateiName)
 ws_openpyxl = wb_openpyxl['Kopfdaten']
 # print(type(wb_openpyxl))
@@ -32,48 +40,11 @@ dictKopfdatenValues = {}
 for keyval, val in dictKopfdatenCells.items():
     inhaltAusZelle = ws_openpyxl[val].value
     dictKopfdatenValues[keyval] = inhaltAusZelle
-    # dictKopfdatenValues = {keyval: inhaltAusZelle keyval, val in dictKopfdatenCells.items()}
-        
-print(dictKopfdatenValues)
-
-# print(type(dictKopfdatenCells.keys()))
 
 
-datumVon = ws_openpyxl['E8'].value
-datumBis = ws_openpyxl['E10'].value
-senderName = ws_openpyxl['E32'].value
-senderIdAuswahl = ws_openpyxl['E34'].value
-senderId =  ws_openpyxl['E36'].value
+if dictKopfdatenValues['datumBis'] > dictKopfdatenValues['datumVon']:
+    print("Prüfung der Datumswerte {0} {1} ist erfolgreich verlaufen.\n\nProzess -Kopfdatenprüfung- kann vorgesetzt werden.".format(dictKopfdatenValues['datumBis'],dictKopfdatenValues['datumVon']))
 
-if datumBis > datumVon:
-    print("Prüfung der Datumswerte {0} {1} ist erfolgreich verlaufen.\n\nProzess -Kopfdatenprüfung- kann vorgesetzt werden.".format(datumVon,datumBis))
 else:
     print("Daten fehlerhaft. Datei wird mit Fehlercode (Datum) in DB geschrieben.")
   
-
-# ws_openpyxl = wb_openpyxl.get_sheet_by_name('Sheet1')
-
-# valueConv =  datetime.strptime(value, '%y/%m/%d')
-# cell_value = worksheet.cell(8, 4).value
-
-# # print(MeldedatumBis)
-# print(test)
-# datum = datetime.fromtimestamp(MeldedatumVon).strftime('%Y-%m-%d')
-# datumBis = datetime.fromtimestamp(MeldedatumBis).strftime('%Y-%m-%d')
-# # print(datum)
-# print(datumBis)
-
-# from pyxlsb import convert_date
-# convert_date(MeldedatumBis)
-# format(convert_date(MeldedatumBis), '%m/%d/%Y')
-# print(MeldedatumBis)
-
-
-# ##########################
-# TEST PANDAS to HANDLE xlsx
-# ##########################
-# df = pd.read_excel(dateiName,sheet_name=None)
-
-# for v in df:
-#     if v != "Bewegungsdaten":
-#         print(v)
