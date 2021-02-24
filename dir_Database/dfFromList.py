d741d10d5a9e810523eba99cdd51598365886c8d
Dave0512 <dschneiderhoffmann@gmail.com>
2021-02-24 14:18:29 +0100

    Type: def - _extractTables if Fehlercode added, var - _lstTabsError

--- dir_Database/dfFromList.py
+++ dir_Database/dfFromList.py
@@ -13,24 +13,35 @@ class ListToDF:
 
     def __init__(self):
         self._excludFiles = FileList().excludedFiles() # Recieves List of files which could not be imported
-    
+        self._lstTabsError = FileList()._filterTabs()[1]
+
+
     def _extractTables(self):
         """
         Input: List of excluded files
         Output: DF with exluded files and inload date
         """
-        seriesExclFiles = pd.Series(self._excludFiles)
-        if seriesExclFiles:
+        lstTabsError = self._lstTabsError
+        if lstTabsError:
+            seriesExclFiles = pd.Series(lstTabsError)
             df = pd.DataFrame({'_AusgeschlDateiPfad_': seriesExclFiles
-                              ,'_date_inload_': str(dt.datetime.now())})
+                               ,'_FehlerCode_': "Erforderliche Tabs nicht vorhanden"
+                               ,'_date_inload_': str(dt.datetime.now())})
             return df
         else:
             print("Jiiieehhhaaa - Keine fehlerhaften Dateien vorhanden.")
-
+        # excludFiles = self._excludFiles 
+        # if excludFiles: # Check if faulty files a
+        #     seriesExclFiles = pd.Series(excludFiles)
+        #     df = pd.DataFrame({'_AusgeschlDateiPfad_': seriesExclFiles
+        #                       ,'_date_inload_': str(dt.datetime.now())})
+        #     return df
+        # else:
+        #     print("Jiiieehhhaaa - Keine fehlerhaften Dateien vorhanden.")
 
 ## TEST
 
-# dfCore = DfExcludedFiles()
+# dfCore = ListToDF()
 # df = dfCore._extractTables()
 # print(df.info())
 # print(df.head())
